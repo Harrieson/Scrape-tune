69acb0f175034012cf15103d436110c4a36b9294
Harrieson <harriemburu@gmail.com>
2025-05-30 18:10:21 +0300

    Prisma and database initialization

---
+++ app/(dashboard)/workflows/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="flex flex-1 flex-col h-full">page</div>
+  )
+}
+
+export default page
---
+++ components/MobileSideber.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MobileSideber() {
+  return (
+    <div>MobileSideber</div>
+  )
+}
+
+export default MobileSideber
